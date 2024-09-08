d17426b6c34f8bbaf8a62920909789d493532eba
TerranceChu <a0321456821@gmail.com>
2024-09-08 17:56:44 +0800

    Create page components

---
+++ src/pages/ApplicationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ApplicationListPage = () => {
+  return (
+    <div>
+      <h1>Application List Page</h1>
+      {/* 显示申请列表 */}
+    </div>
+  );
+};
+
+export default ApplicationListPage;
